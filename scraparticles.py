21510bba0239784d4279bbb8b3eec2e26c6660b7
Chip <chip@freyhuus.com>
2018-09-26 09:14:52 +0200

    dbutils for encapsulating db actions csv files now written for categories, article-categories and articles

--- scraparticles.py
+++ scraparticles.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python
 # coding: utf-8
 
+import csv
+import writearticlescsv
+import scrapdescription
+
+try:
+    from urllib import unquote
+except ImportError:
+    from urllib.parse import unquote
+
 import requests
 from lxml import etree
 from requests import Response
 
-def parse_articles(fullUrl):
+def parse_articles(fullUrl, subCategoryId):
     myparser = etree.HTMLParser(encoding="utf-8")
     item = requests.get(fullUrl)  # type: Response
     tree = etree.HTML(item.content, myparser)
@@ -13,7 +22,16 @@ def parse_articles(fullUrl):
 
     # print descriptions
     theOutput = open('articles.out', 'a')
-    theOutput.write('\n' + fullUrl + '\n')
+    theheading = unquote(fullUrl.split('/')[-2])
+    theOutput.write('\n' + fullUrl + '\n' + theheading + '\n')
+    # The first column is the category id
+    theOutput.write('categoryId;')
+
+    # entry = {'categoryId': str(subCategoryId), 'parentID': mainCategory['categoryId'], 'name': textForCSV,
+    #          'active': str(1)}
+    #
+    # entry = {'categories':str(subCategoryId)}
+    # entry = {'ordernumber':str(),'mainnumber','name','description_long','categories','propertyValueName','imageUrl']
     for description in columns:
         #    sDesc = description.decode('UTF-8')
         #    theOutput.write(sDesc)
@@ -25,6 +43,8 @@ def parse_articles(fullUrl):
     #     "//*[re:test(local-name(), '^TEXT.*')]",
     #     namespaces={'re': "http://exslt.org/regular-expressions"})
 
+    mainpagename = tree.xpath('//div[@class="mainpagename"]/h1/text()')[0]
+
     articleNrs = tree.xpath('//table[@class="articles"]/tr[starts-with(@class,"stdrow")]/td/span/text()')
 
     # articleNr = tree.xpath('//table[@class="articles"]/tr[re:test(local-name(),".*stdrow1.*"])]/td/span/text()',
@@ -33,18 +53,41 @@ def parse_articles(fullUrl):
     # first all the rows
     rows = tree.xpath('//table[@class="articles"]/tr[starts-with(@class,"stdrow")]/td/text()')
 
-    articlePos = 0
-    presentColumn = 0
-    if len(rows) == 1:
-        theOutput.write("\nFound a single item\n")
-    for row in rows:
-        if presentColumn == 0:
-            theOutput.write(articleNrs[articlePos] + ';')
-            articlePos = articlePos + 1
-        theOutput.write(row.strip() + ';')
-        presentColumn = presentColumn + 1
-        if presentColumn == numColumns - 1:
-            theOutput.write('\n')
-            presentColumn = 0
+    with open(file='article-categories.csv', mode='a', encoding='UTF-8') as csvfile:
+        fieldnames = ['ordernumber','mainnumber','categoryId']
+        filewriter = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';',quotechar='"')
+
+
+        articlePos = 0
+        presentColumn = 0
+        description_long = scrapdescription.parse_description(fullUrl,subCategoryId)
+        # the image is only for the first item on the page
+        imageUrl = 'https://www.freyhuus.com/images/marintecShopware/' + articleNrs[0][:2] + '/' + articleNrs[0] + '.jpg'
+
+        values = {}
+        if len(rows) == 1:
+            theOutput.write("\nFound a single item\n")
+        for row in rows:
+            if presentColumn == 0:
+                theOutput.write('trying to get pos ' + str(articlePos) + ' from ' + str(articleNrs) + 'and present column '+ str(presentColumn) +'\n')
+                articleNumber = articleNrs[articlePos]
+                theOutput.write('getting ' + str(articleNumber) + ' at pos '+str(articlePos) + '\n')
+                theOutput.write(row)
+                entry = {'ordernumber': articleNumber,'mainnumber': articleNumber,
+                         'categoryId': str(subCategoryId/100)} # category from the parent
+                filewriter.writerow(entry)
+                theOutput.write(str(subCategoryId) + ';' + articleNumber + ';' )
+                articlePos += 1
+            entry['name'] = mainpagename
+            entry['description_long'] = description_long
+            entry['imageUrl'] = imageUrl
+            values[columns[presentColumn+1]] = row.strip()
+            theOutput.write(row.strip() + ';')
+            presentColumn = presentColumn + 1
+            if presentColumn == numColumns - 1:
+                entry['values'] = values
+                theOutput.write('\n')
+                writearticlescsv.write_article(entry)
+                presentColumn = 0
 
 
--- scrapcategories.py
+++ scrapcategories.py
@@ -17,7 +17,7 @@ def parse_categories(domain):
     db.commit()
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS categories(categoryId INTEGER PRIMARY KEY, name TEXT,
-                           parentId INTEGER)
+                           parentID INTEGER)
     ''')
     db.commit()
 
@@ -38,14 +38,14 @@ def parse_categories(domain):
         #    theOutput.write(sDesc)
         link = category.attrib['href']
         text = category.text
-        allCategories.append({'link':link, 'text':text, 'categoryId':mainCategoryId, 'parentId':str(3),'name':text})
+        allCategories.append({'link':link, 'text':text, 'categoryId':mainCategoryId, 'parentID':str(3),'name':text})
         allCategoriesForCSV.append(
             {'categoryId': mainCategoryId, 'parentID': str(3), 'name': text, 'active': str(1)})
 
         theOutput.write("Id <"+ str(mainCategoryId) + "> Remote <"+text + "> with the href: " + link + "\n")
-        cursor.execute('''INSERT INTO categories(categoryId, parentId, name)
-                      VALUES(:categoryId,:parentId, :name)''',
-                       {'categoryId': mainCategoryId, 'parentId': 3, 'name': text})
+        cursor.execute('''INSERT INTO categories(categoryId, parentID, name)
+                      VALUES(:categoryId,:parentID, :name)''',
+                       {'categoryId': mainCategoryId, 'parentID': 3, 'name': text})
         db.commit()
         mainCategoryId = mainCategoryId + 1
     with open(file='categories.csv', mode='w', encoding='UTF-8') as csvfile:
@@ -53,7 +53,7 @@ def parse_categories(domain):
                       'cmsheadline', 'cmstext', 'template', 'active', 'blog', 'external', 'hidefilter',
                       'attribute_attribute1', 'attribute_attribute2', 'attribute_attribute3', 'attribute_attribute4',
                       'attribute_attribute5', 'attribute_attribute6', 'CustomerGroup']
-        filewriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        filewriter = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';',quotechar='"')
         filewriter.writeheader()
         filewriter.writerows(allCategoriesForCSV)
     db.commit()
